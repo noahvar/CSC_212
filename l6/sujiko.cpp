9f870c30aa5a6a1469f3f7ce7087ebe8cde26284
Noah Vargas <noahvar23@gmail.com>
2024-10-19 12:50:55 -0400

    sujiko finished

--- l6/sujiko.cpp
+++ l6/sujiko.cpp
@@ -42,33 +42,45 @@ Sujiko::~Sujiko() {
 }
 
 bool Sujiko::solve(unsigned int cell){
-    // If we placed a number succesfully on every cell, we did it!
+    // If we placed a number successfully on every cell, we did it!
     //0.) Is the win condition met? If so, return true. Otherwise, continue on.
-    if(cell == 8)
+    if(cell == 9)
     {
         return isValid();
     }
+
+    //1.) At any given step, record all of the missing values.
+    std::vector<unsigned int> missing = {1,2,3,4,5,6,7,8,9};
+    for(int r = 0; r < 3; r++)
+    {
+        for(int c = 0; c < 3; c++)
+        {
+            if(board[r][c] != 0)
+            {
+                missing.erase(std::remove(missing.begin(), missing.end(), board[r][c]));
+            }
+        }
+    }
+
+    //1.2) Place the first missing value into the first open spot on the board.
 
     //get row and col from cell
     unsigned int row = cell/3;
     unsigned int col = cell%3;
 
-    //1.) At any given step, record all of the missing values.
-    std::vector<unsigned int> missing = {1,2,3,4,5,6,7,8,9};
-    for(int row = 0; row < 3; row++)
+    //check if the current cell is filled
+    if(board[row][col] != 0)
     {
-        for(int col = 0; col < 3; col++)
-        {
-            if(board[row][col] != 0)
-            {
-                missing.erase(std::remove(missing.begin(), missing.end(), board[row][col]));
-            }
-        }
+        //recursive call to move to next cell if this one is filled
+        return solve(cell+1);
     }
-    //1.2) Place the first missing value into the first open spot on the board.
-    for(int element : missing)
+
+    for(unsigned int element : missing)
     {
         board[row][col] = element;
+        //debug to show the board
+        display_board();
+        std::cout << std::endl;
         //1.3) Recursively call this function with the new board. If we obtain True, also return True.
         if(solve(cell+1))
         {
@@ -90,19 +102,7 @@ bool Sujiko::isValid()
     unsigned int topRightCheck = board[0][1]+board[0][2]+board[1][1]+board[1][2];
     unsigned int botLeftCheck = board[1][0]+board[1][1]+board[2][0]+board[2][1];
     unsigned int botRightCheck = board[1][1]+board[1][2]+board[2][1]+board[2][2];
-    if(topLeft == topLeftCheck)
-    {
-        return true;
-    }
-    if(topRight == topRightCheck)
-    {
-        return true;
-    }
-    if(botLeft == botLeftCheck)
-    {
-        return true;
-    }
-    if(botRight == botRightCheck)
+    if(topLeft == topLeftCheck && topRight == topRightCheck && botLeft == botLeftCheck && botRight == botRightCheck)
     {
         return true;
     }
